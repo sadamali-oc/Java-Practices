d9293704abf2820c0deb7ad3882593864b3aeb9b
sadamali-oc <110493084+sadamali-oc@users.noreply.github.com>
2024-09-14 21:46:38 +0530

    abstraction oop concept

--- src/Kamal.java
+++ src/Kamal.java
@@ -1,4 +1,21 @@
 
 //abstract class there be one or more abstract methods
-public class Kamal {
+
+//abstract method - without body (it means no implementation)
+abstract public class Kamal {
+
+   abstract void run();
+
+
+   void  eat(){
+       System.out.println("eating");
+   }
+
+
+
+
+
+
 }
+
+
--- src/Nimal.java
+++ src/Nimal.java
@@ -1,3 +1,22 @@
-public class Nimal {
+public class Nimal extends  Kamal {
+
+
+    @Override
+    void run() {
+        System.out.println("hello");
+    }
+
+    void drink(){
+
+        System.out.println("drinking");
+    }
+    public static void main(String[] args) {
+        Nimal obj1 = new Nimal();
+        obj1.drink();
+
+        Nimal obj2=new Nimal();
+        obj2.run();
+        obj2.eat();
+    }
 
 }
