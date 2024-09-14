628513ce03cc7d90e08bc1de7b7f4ef404e6285d
sadamali-oc <110493084+sadamali-oc@users.noreply.github.com>
2024-09-14 22:00:15 +0530

    Create Loop.java
    while loop

---
+++ src/Loop.java
@@ -0,0 +1,10 @@
+public class Loop {
+    public static void main(String[] args) {
+
+        int x =1;
+        while (x <=4){
+            System.out.println(x);
+            x++;
+        }
+    }
+}
