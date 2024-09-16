3504df61feefb70b31338dea7f4596cca169fc79
sadamali-oc <110493084+sadamali-oc@users.noreply.github.com>
2024-09-16 13:33:27 +0530

    inheritance

---
+++ src/Father.java
@@ -0,0 +1,12 @@
+public class Father {
+
+
+
+    int noOfAccount =4;
+    public  void show(){
+
+        System.out.println("Blance amount is 540000/=");
+
+    }
+
+}
---
+++ src/Son.java
@@ -0,0 +1,9 @@
+public class Son  extends Father{
+
+
+    public static void main(String[] args) {
+        Son a=new Son();
+        System.out.println("No of Accounts :" + a.noOfAccount);
+
+    }
+}
