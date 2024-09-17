bbca122efd7df58e2c3bb8bbe869f91b9599471a
sadamali-oc <110493084+sadamali-oc@users.noreply.github.com>
2024-09-18 04:33:51 +0530

    turnory operator

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,8 @@
-class Main {
-    public  static void main(String  args[]){
-
-        int b=4;
-        int c=32;
-        boolean result = b<c;
-
-
-
-        System.out.println(result);
-    }
-
-}
+//class Main {
+//    public  static void main(String  args[]){
+//
+//
+//
+//
+//
+//}
---
+++ src/TernaryOperator.java
@@ -0,0 +1,7 @@
+public class TernaryOperator {
+    public static void main(String[] args) {
+        int  n=7;
+      String total= n%2==0 ? "even" : "odd";
+        System.out.println(total);
+    }
+}
