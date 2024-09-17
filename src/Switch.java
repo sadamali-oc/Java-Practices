2435a56ff8f9e9eb66c6da74a59bc0e0e91dc184
sadamali-oc <110493084+sadamali-oc@users.noreply.github.com>
2024-09-18 04:36:20 +0530

    Create Switch.java

---
+++ src/Switch.java
@@ -0,0 +1,17 @@
+public class Switch {
+    public static void main(String[] args) {
+        int n=1;
+
+        switch (n){
+            case 1:
+                System.out.println(1);
+                break;
+
+            case 2:
+                System.out.println(2);
+                break;
+            default:
+                System.out.println("hi");
+        }
+    }
+}
