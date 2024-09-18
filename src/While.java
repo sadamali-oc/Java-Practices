ed8e7624d00aa09289c1f732cd0c2f30b6c0f378
sadamali-oc <110493084+sadamali-oc@users.noreply.github.com>
2024-09-18 16:41:23 +0530

    Update While.java

--- src/While.java
+++ src/While.java
@@ -1,10 +1,26 @@
 public class While {
     public static void main(String[] args) {
 
-        int x=0;
-        while(x<5){
-            System.out.println("hi");
-            x++;
+//        int x=0;
+//        while(x<5){
+//            System.out.println("hi");
+//            x++;
+//        }
+//
+
+
+        int i=1;
+        while (i<=5){
+
+            System.out.println("The day"+ i);
+            int j=1;
+            while (j<=3){
+                System.out.println("good");
+                j++;
+            }
+            i++;
+
         }
+
     }
 }
