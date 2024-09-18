d7ef1078b9bf873972c0b78b0615f7391b23ad4d
sadamali-oc <110493084+sadamali-oc@users.noreply.github.com>
2024-09-18 16:18:00 +0530

    Create While.java

---
+++ src/While.java
@@ -0,0 +1,10 @@
+public class While {
+    public static void main(String[] args) {
+
+        int x=0;
+        while(x<5){
+            System.out.println("hi");
+            x++;
+        }
+    }
+}
