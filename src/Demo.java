6e8bbb61be01059d201c13f4896af1c6b0767e88
sadamali-oc <110493084+sadamali-oc@users.noreply.github.com>
2024-09-18 19:07:17 +0530

    created object

--- src/Demo.java
+++ src/Demo.java
@@ -1,5 +1,24 @@
+class Calculator{
+
+    public  int add(int n1,int n2){
+        int r=n1+n2;
+
+        return r;
+    }
+
+}
+
+
+
 public class Demo {
+
     public static void main(String[] args) {
         
+    Calculator calc = new Calculator() ;
+    int result= calc.add(24,356);
+    System.out.println(result);
+
+
+
     }
 }
