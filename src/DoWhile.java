bcdcf00763aa4e328b8d5b8bd89569d534773412
sadamali-oc <110493084+sadamali-oc@users.noreply.github.com>
2024-09-18 16:52:44 +0530

    Create DoWhile.java

---
+++ src/DoWhile.java
@@ -0,0 +1,13 @@
+public class DoWhile {
+
+    public static void main(String[] args) {
+
+        int i=1;
+       do{
+
+            System.out.println("The day"+ i);
+i++;
+        } while (i<=5);
+    }
+}
+
