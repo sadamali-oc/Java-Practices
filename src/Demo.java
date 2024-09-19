1a651d8c8431f0e61311d4383cdf68fb6439ad25
sadamali-oc <110493084+sadamali-oc@users.noreply.github.com>
2024-09-19 22:53:27 +0530

    Update Demo.java

--- src/Demo.java
+++ src/Demo.java
@@ -1,8 +1,16 @@
 class Calculator{
 
+
+int num;
+    //n1, n2 are local variables
     public  int add(int n1,int n2){
         int r=n1+n2;
 
+        return r;
+    }
+
+    public  int add(int n1,int n2,int n3){
+        int r=n1+n2+n3;
         return r;
     }
 
@@ -14,10 +22,16 @@ public class Demo {
 
     public static void main(String[] args) {
 
+
+        int a =10;
+        
     Calculator calc = new Calculator() ;
     int result= calc.add(24,356);
     System.out.println(result);
 
+
+    int r=calc.add(23,43,12);
+        System.out.println(r);
 
 
     }
