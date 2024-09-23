287ed574c828c9b8844c94438858de06134421e5
sadamali-oc <110493084+sadamali-oc@users.noreply.github.com>
2024-09-23 23:00:30 +0530

    Create Array2.java

---
+++ src/Array2.java
@@ -0,0 +1,25 @@
+public class Array2 {
+    public static void main(String[] args) {
+        int num[][]=new int[3][4];
+
+       int random =0;
+
+
+        for(int i=0;i<3;i++){
+            for(int j=0;j<4;j++){
+
+             num[i][j] =  (int) (Math.random()*10);
+
+            }
+        }
+
+
+        for(int i=0;i<3;i++){
+            for(int j=0;j<4;j++){
+                System.out.print(num[i][j] + " ");
+            }
+            System.out.println(" ");
+        }
+
+    }
+}
